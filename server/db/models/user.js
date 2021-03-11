0fd7a91cb65e057338ba364462fe0828bc3b9c27
Kahoua (Kay) XiongPachay <kayxxx2020@gmail.com>
2021-03-11 11:56:37 -0500

    BE: Creates User Model

---
+++ server/db/models/user.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+//User Model
+const User = db.define("user", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
+module.exports = User;
