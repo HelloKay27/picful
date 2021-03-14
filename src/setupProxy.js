f96e38a1a12cd4949089026dd9e6a58c56055033
Kahoua (Kay) XiongPachay <kayxxx2020@gmail.com>
2021-03-14 14:50:02 -0400

    updates proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,18 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    proxy("/api/*", {
-      target: "http://picful.herokuapp.com:4000",
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:4000",
       secure: false,
       changeOrigin: true,
     })
   );
   app.use(
-    proxy("/auth/*", {
-      target: "http://picful.herokuapp.com:4000",
+    "/auth",
+    createProxyMiddleware({
+      target: "http://localhost:4000",
       secure: false,
       changeOrigin: true,
     })
