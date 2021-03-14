6a03e56f5e3514711a6bf5bf165257b40af91840
Kahoua (Kay) XiongPachay <kayxxx2020@gmail.com>
2021-03-14 14:11:28 -0400

    fixed database

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,9 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
 
-module.exports = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
-  logging: false,
-});
+module.exports = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
+  {
+    logging: false,
+  }
+);
