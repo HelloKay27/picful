be3c66c05b1ac4438a1f69ac33c764684e1d3f22
Kahoua (Kay) XiongPachay <kayxxx2020@gmail.com>
2021-03-14 14:37:13 -0400

    changes to setup Proxy target

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,14 +3,14 @@ const proxy = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     proxy("/api/*", {
-      target: "http://localhost:4000/",
+      target: "https://localhost:4000",
       secure: false,
       changeOrigin: true,
     })
   );
   app.use(
     proxy("/auth/*", {
-      target: "http://localhost:4000/",
+      target: "https://localhost:4000",
       secure: false,
       changeOrigin: true,
     })
